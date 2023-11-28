6f352df0b6d9f6adc3a51432816c119ffcb77ee9
SKGakiria <swgakiria@gmail.com>
2023-11-28 10:03:43 +0000

    Added Task 2 - Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/redis-6.0.10/1-redis_op.js
@@ -0,0 +1,28 @@
+// Script to connect Redis server
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, (err, reply) => {
+    console.log(reply);
+  });
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
