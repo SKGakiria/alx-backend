c70849c82c1a67bb8608b35c9d8a6824fbcf1ab1
SKGakiria <swgakiria@gmail.com>
2023-11-29 07:20:57 +0000

    Task 6 - Create the Job creator

---
+++ 0x03-queuing_system_in_js/redis-6.0.10/6-job_creator.js
@@ -0,0 +1,22 @@
+// Creates a Job creator
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '020543347896',
+  message: 'Account registration successful',
+});
+
+job
+  .on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+
+job.save();
